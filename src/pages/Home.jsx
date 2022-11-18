ca4ace9af29c3d3dcf227c5262152538229d8f47
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-11-19 01:28:25 +0900

    init : 초기세팅

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+function Home() {
+  return (
+    <StHomeContainer>
+      <h1>안녕하세요</h1>
+    </StHomeContainer>
+  );
+}
+
+export default Home;
+
+const StHomeContainer = styled.section``;
