82f701ce0c6a89fd4ea3f7b8f0eefa28dc510bf0
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-11-20 00:43:40 +0900

    feat : 라우터 설정

--- src/components/common/Router.jsx
+++ src/components/common/Router.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../../pages/Home';
+import QuizStart from '../../pages/QuizStart';
+import QuizIng from '../../pages/QuizIng';
+import QuizEnd from '../../pages/QuizEnd';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/quizstart" element={<QuizStart />} />
+        <Route path="/quizIng" element={<QuizIng />} />
+        <Route path="/quizEnd" element={<QuizEnd />} />
       </Routes>
     </BrowserRouter>
   );
