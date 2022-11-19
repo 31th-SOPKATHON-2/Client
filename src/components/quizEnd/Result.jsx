86c1335d39d62940e22a5f99a7af0f94c5081591
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-11-20 03:43:53 +0900

    feat : 퀴즈 결과 퍼블리싱

--- src/components/quizEnd/Result.jsx
+++ src/components/quizEnd/Result.jsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { theme } from '../../styles/theme';
 
-function Result() {
+function Result({ resultList }) {
+  const [score, setScore] = useState(0);
+  const { img, title, desctitle, descvalue1, descvalue2 } = resultList[score];
+  let result = 7;
+
+  useEffect(() => {
+    handleScore();
+  }, []);
+
+  const handleScore = () => {
+    if (result === 0) {
+      setScore(3);
+    } else if (result > 0 && result < 4) {
+      setScore(2);
+    } else if (result > 3 && result < 7) {
+      setScore(1);
+    } else {
+      setScore(0);
+    }
+  };
+
   return (
     <StResultContainer>
       <h1>당신의 정신연령은</h1>
-      <div></div>
+      <StResultImg src={img} alt="결과이미지" />
+      <StResultTitle src={title} alt="결과타이틀" />
+      <span>{desctitle}</span>
+      <StDescriptionContainer>
+        <h2>잼민이 Type 설명</h2>
+        <p>{descvalue1}</p>
+        <p>{descvalue2}</p>
+      </StDescriptionContainer>
     </StResultContainer>
   );
 }
@@ -18,7 +46,7 @@ const StResultContainer = styled.div`
   justify-content: center;
   align-items: center;
 
-  h1 {
+  & > h1 {
     margin-bottom: 2.4rem;
 
     font-weight: ${theme.fontWeight.medium};
@@ -26,9 +54,46 @@ const StResultContainer = styled.div`
     line-height: 140%;
   }
 
-  div {
-    width: 20rem;
-    height: 20rem;
-    background-color: ${theme.colors.jemminiBlue};
+  & > span {
+    margin-bottom: 1.6rem;
+
+    font-weight: ${theme.fontWeight.light};
+    font-size: ${theme.fontSize.body2};
+    line-height: 150%;
+  }
+`;
+
+const StResultImg = styled.img`
+  margin-bottom: 1rem;
+
+  width: 14.8rem;
+  height: 14.8rem;
+`;
+const StResultTitle = styled.img`
+  width: 26rem;
+  height: 5rem;
+`;
+
+const StDescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  margin-bottom: 8rem;
+
+  & > h2 {
+    margin-bottom: 1.6rem;
+
+    color: ${theme.colors.black};
+    font-weight: ${theme.fontWeight.medium};
+    font-size: ${theme.fontSize.heading5};
+  }
+
+  & > p {
+    text-align: center;
+    font-weight: ${theme.fontWeight.light};
+    font-size: ${theme.fontSize.body2};
+    line-height: 150%;
   }
 `;
--- src/pages/QuizEnd.jsx
+++ src/pages/QuizEnd.jsx
@@ -2,12 +2,54 @@ import styled from 'styled-components';
 import closeIcon from '../assets/icon/icon_close.svg';
 import { theme } from '../styles/theme';
 import Result from '../components/quizEnd/Result';
+import { useNavigate } from 'react-router-dom';
+import resultTitleOne from '../assets/image/img_result-title-one.svg';
+import resultImgOne from '../assets/image/img_result-img-one.svg';
+import resultTitleTwo from '../assets/image/img_result-title-two.svg';
+import resultImgTwo from '../assets/image/img_result-img-two.svg';
+import resultTitleThree from '../assets/image/img_result-title-three.svg';
+import resultImgThree from '../assets/image/img_result-img-three.svg';
+import resultTitleFour from '../assets/image/img_result-title-four.svg';
+import resultImgFour from '../assets/image/img_result-img-four.svg';
 
 function QuizEnd() {
+  const navigate = useNavigate();
+  const resultList = [
+    {
+      img: resultImgOne,
+      title: resultTitleOne,
+      desctitle: '잼민이 그 잡체네요 쫑받게.',
+      descvalue1: '당신은 현직 퓨어 잼민이급 언어를 구사합니다.',
+      descvalue2: '벽이 느껴지네요, 완벽',
+    },
+    {
+      img: resultImgTwo,
+      title: resultTitleTwo,
+      desctitle: '생각보다 좀 치네?',
+      descvalue1: '제법 트렌드에 민감한 당신,',
+      descvalue2: '조금 더 노력해서 완벽한 잼민이가 되어보아요',
+    },
+    {
+      img: resultImgThree,
+      title: resultTitleThree,
+      desctitle: '어쩔 킹저씨?',
+      descvalue1: '할말하않 당신은 딱 킹받는 아저씨 수준이에요.',
+      descvalue2: '할말하앉, 당신은 킹받는 아조씨에요.',
+    },
+    {
+      img: resultImgFour,
+      title: resultTitleFour,
+      desctitle: '여기는 젊은 친구들 노는 곳이에요.',
+      descvalue1: '요즘 친구들 참 어렵죠?',
+      descvalue2: '번역기로 공부하세요.',
+    },
+  ];
+
   return (
     <StQuizEndContainer>
-      <StCloseIcon src={closeIcon} alt="닫기버튼" />
-      <Result />
+      <StCloseIcon src={closeIcon} alt="닫기버튼" onClick={() => navigate('/')} />
+      <Result resultList={resultList} />
+      <StRestartBtn onClick={() => navigate('/quizIng')}>다시하기</StRestartBtn>
     </StQuizEndContainer>
   );
 }
@@ -23,4 +65,17 @@ const StQuizEndContainer = styled.section`
 
 const StCloseIcon = styled.img`
   margin: 7.4rem 3rem 5.4rem 32.1rem;
+`;
+
+const StRestartBtn = styled.button`
+  width: 17.9rem;
+  height: 5.2rem;
+
+  background-color: ${theme.colors.jemminiBlue};
+  border-radius: ${theme.radius.radius};
+
+  color: white;
+  font-weight: ${theme.fontWeight.medium};
+  font-size: ${theme.fontSize.heading5};
+  line-height: 140%;
 `;
