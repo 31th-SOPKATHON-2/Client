121a3cacd92eb7ce0ac7139beda8480ea4a76453
Seong-Gyeong <heather128@ewhain.net>
2022-11-20 02:52:32 +0900

    feat: 검색 결과 뷰 구현

--- src/components/HomeResult.jsx
+++ src/components/HomeResult.jsx
@@ -1,17 +1,24 @@
 import styled from 'styled-components';
 import { theme } from '../styles/theme.js';
-import HomeHeader from '../components/HomeHeader.jsx';
-import HomeSearchBtn from '../components/HomeSearchBtn.jsx';
-import HomeQuizBtn from '../components/HomeQuizBtn.jsx';
+import HomeHeader from './HomeHeader.jsx';
+// import HomeSearchBtn from './HomeSearchBtn.jsx';
+import HomeQuizBtn from './HomeQuizBtn.jsx';
 
 export default function HomeResult() {
   return (
     <>
-      <HomeHeader />
-      <HomeQuizBtn type="button" />
       <StResultContainer>
-        <StResultMeaningContainer></StResultMeaningContainer>
-        <StExample>개킹받네</StExample>
+        <HomeHeader />
+        <HomeQuizBtn type="button" />
+
+        <StMeaningContainer>
+          <StMeaingContainerTitle>"킹받네"의 번역</StMeaingContainerTitle>
+          <StMeaningOutput>열 받는다</StMeaningOutput>
+          <StMeaingOutputDesc>어쩌구저쩌구</StMeaingOutputDesc>
+        </StMeaningContainer>
+        <StExampleWrap>
+          <StExample>개킹받네</StExample>
+        </StExampleWrap>
         <StExampleMessage>예문 누르면 복사됌 ㅋ</StExampleMessage>
       </StResultContainer>
     </>
@@ -25,27 +32,73 @@ const StResultContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
-  width: 30.5rem;
-  height: 12.6rem;
 `;
 
-const StResultMeaningContainer = styled.div`
+const StMeaningContainer = styled.div`
+  margin-top: 2rem;
+
   width: 30.5rem;
   height: 12.6rem;
 
   border: 1px solid ${theme.colors.grey200};
+
+  border-radius: ${theme.radius.radius};
+`;
+
+const StMeaingContainerTitle = styled.div`
+  width: 76px;
+  height: 18px;
+
+  margin-left: 1.5rem;
+  margin-top: 1.6rem;
+
+  color: ${theme.colors.jemminiRed};
+
+  font-size: ${theme.fontSize.description};
+  font-weight: ${theme.fontWeight.light};
+`;
+
+const StMeaningOutput = styled.div`
+  margin-left: 1.6rem;
+  margin-top: 0.2rem;
+
+  width: 77px;
+  height: 28px;
+
+  font-size: ${theme.fontSize.heading4};
+  font-weight: ${theme.fontWeight.medium};
+`;
+
+const StMeaingOutputDesc = styled.div`
+  margin-left: 1.5rem;
+  margin-top: 1rem;
+
+  width: 212px;
+  height: 36px;
+
+  font-size: ${theme.fontSize.description};
+  font-weight: ${theme.fontWeight.light};
+`;
+const StExampleWrap = styled.div`
+  margin-top: 1.2rem;
+
+  width: 30.5rem;
+  height: 4.4rem;
+
+  display: flex;
+  align-items: center;
+
+  border: 1px solid ${theme.colors.jemminiBlue};
+
+  border-radius: ${theme.radius.radius};
 `;
 
 const StExample = styled.div`
-  width: 30.5rem;
-  height: 4.4rem;
+  margin-left: 1.6rem;
 
   color: ${theme.colors.black};
   font-size: ${theme.fontSize.body1};
   font-weight: ${theme.fontWeight.light};
-
-  border: 1px solid ${theme.colors.jemminiBlue};
 `;
 
 const StExampleMessage = styled.div`
